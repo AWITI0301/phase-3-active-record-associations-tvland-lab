79b4136c8c3529fa621577a590c8fc09990f9858
AWITI0301 <johnoketchawiti@gmail.com>
2022-09-13 09:45:24 +0300

    commit tree 11c41ecc7aac459a173b78ac006e651b41b82d82

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,13 @@
-class Network < ActiveRecord::Base
-  
+class Show < ActiveRecord::Base
+    has_many :characters
+    belongs_to :network
+    has_many :actors, through: :characters
+
+    def actors_list
+        empty = []
+        self.actors.each do |act|
+            empty << "#{act.first_name} #{act.last_name}"
+        end
+        empty
+    end
 end
---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+
+# emilia = Actor.create(first_name: "Emilia", last_name: "Clarke")
+# got = Show.create(name: "Game of Thrones")
+# Character.create(name: "Khaleesi", actor_id: emilia.id, show_id: got.id)
+# peter = Actor.create(first_name: "Peter", last_name: "Dinklage")
+# thrones = Show.create(name: "Game of Thrones")
+# tyrion = Character.create(name: "Tyrion Lannister", actor_id: peter.id, show_id: thrones.id)
